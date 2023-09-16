ddbf3230d2939717d23dce15bcf56d81b1da228f
Karinkarllina <115635082+Karinkarllina@users.noreply.github.com>
2023-09-16 16:50:52 +0000

    Deploying to gh-pages from @ Karinkarllina/WellGo@62ce3efc402dfcf7de407ad2fe019b4f5b444252 🚀

---
+++ static/js/339.9b4da462.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[339],{339:function(e,t,a){a.r(t);var r=a(87),l=a(184);t.default=function(){return(0,l.jsxs)("div",{children:[(0,l.jsx)("p",{children:"Hello! Its FavoritePage!"}),(0,l.jsx)("p",{children:(0,l.jsx)(r.OL,{to:"/catalog",children:"Link CatalogPage"})})]})}}}]);
+//# sourceMappingURL=339.9b4da462.chunk.js.map
