6a8d018ba6e9ac8b54eb3466532b96e15b74131f
Karinkarllina <karinkhaliullina@gmail.com>
2023-09-18 01:58:28 +0300

    Вернула

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ const NotFound = lazy(() => import('../pages/NotFound'));
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/wellGo">
+    <BrowserRouter basename="/WellGo">
         <Suspense fallback={<Spinner />}>
             <Routes>
               <Route path='/' element={<Layout/>}> 
