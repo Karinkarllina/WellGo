fa993f7a695427c8c4953f441e57d5df52aba182
Karinkarllina <karinkhaliullina@gmail.com>
2023-09-18 01:13:42 +0300

    TEST

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -32,11 +32,9 @@ export default function Catalog() {
             });
         setFavouriteCars(JSON.parse(localStorage.getItem('items'))); 
 
-        if (favouriteCars) {
-            setFavouriteCars(favouriteCars);
-        }
 
-    }, [favouriteCars]);
+
+    }, []);
 
 
     useEffect(() => { 
